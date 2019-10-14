b84465f35edc467036580bb021500714cee6267d
Matthijs van Henten <mvhenten@users.noreply.github.com>
2019-10-14 18:34:16 +0200

    Add english words

--- js/data/en.js
+++ js/data/en.js
@@ -1,127 +1,82 @@
 import Assignments from "../lib/assignments.js";
 
 const raw = `
-boven -- above 
-opnieuw -- again 
-levend -- alive 
-ook -- also 
-artikel -- article 
-op -- at 
-tante -- aunt 
-slecht -- bad 
-Bangladesh -- Bangladesh 
-groot -- big 
-beide -- both 
-bowlen -- bowling 
-broer -- brother 
-bezig -- busy 
-kopen -- to buy 
-tekenfilm -- cartoon 
-kinderen -- children 
-stad -- city / town 
-land -- country 
-neef / nicht -- cousin 
-dochter -- daughter 
-dag -- day 
-dood -- dead 
-verschillend -- different 
-hond -- dog 
-tekenen -- to draw 
-rijden -- to drive 
-taak -- duty 
-ieder -- each 
-elk -- every 
-fabriek -- factory 
-familie -- family 
-familieboom -- family tree 
-vader -- father 
-invullen -- to fill in 
-first lady -- first lady 
-vorm -- form 
-Frans -- French 
-fruit -- fruit 
-gaan winkelen -- to go shopping 
-grootvader -- grandfather 
-grootmoeder -- grandmother 
-grootouder -- grandparent 
-kruidenierswinkel -- grocery 
-gelukkig -- happy 
-hebben -- to have 
-vechten -- to have a fight 
-hem -- him 
-huis -- house 
-echtgenoot -- husband 
-belangrijk -- important 
-baan -- job 
-dame -- lady 
-wonen -- to live 
-houden van -- to love 
-ontmoeten -- to meet 
-midden -- middle 
-duizenden -- millions 
-ontbrekend -- missing 
-geld -- money 
-meer -- more 
-moeder -- mother 
-dichtbij -- near 
-geopend -- open 
-eigen -- own 
-paragraaf -- paragraph 
-mensen -- people 
-Portugees / Portugese -- Portuguese 
-cadeau -- present 
-president -- president 
-project -- project 
-huren -- to rent 
-kamer -- room 
-sparen -- to save 
-school -- school 
-secretaresse -- secretary 
-zien -- to see 
-verkopen -- to sell 
-winkel -- shop 
-winkelen -- shopping 
-zus -- sister 
-ruimte -- space 
-spelling -- spelling 
-student -- student 
-studeren -- to study 
-ruilen -- to swap 
-leraar / lerares -- teacher 
-tiener -- teenage 
-twee -- two 
-oom -- uncle 
-tot -- until 
-erg / heel -- very 
-water -- water 
-afstand -- way 
-echtgenote -- wife 
-vrouw -- woman 
-werken -- to work 
-zo'n -- such 
-Zwitsers / Zwitser -- Swiss 
-meenemen -- to take 
-to tell -- vertellen
-terrible -- verschrikkelijk
-their -- hun
-them -- hen
-theme park -- pretpark
-they -- zij
-to think -- denken
-three -- drie
-time -- tijd
-title -- titel
-today -- vandaag
-together -- samen
-to try -- proberen
-to underline -- onderstrepen
-us -- ons
-to want -- willen
-we -- wij
-when -- wanneer
-you -- jij / u
-your -- jouw / uw
-yourself -- jezelf / uzelf
+; Family
+tante -- aunt
+vriendje -- boyfriend
+nicht, neef -- cousin
+vriendinnetje -- girlfriend
+kleindochter -- granddaughter
+kleinzoon -- grandson
+halfbroer -- half brother
+man -- husband
+neef -- nephew
+nicht -- niece
+broers en zussen -- siblings
+stiefvader -- stepfather
+stiefmoeder -- stepmother
+tweelingen -- twins
+oom -- uncle
+vrouw, echtgenote -- wife
+; Dates and things
+adres -- address
+verjaardag -- birthday
+geboren -- born
+Brits -- British
+voornaam -- first name
+mobiele telfoonnummer -- mobile phone number
+nationaliteit -- nationality
+persoonlijke gegevens -- personal information
+postcode -- postcode
+; On holiday
+in het buitenland -- abroad
+verbazingwekkend -- amazing
+saai -- boring
+ravijn -- canyon
+vies -- dirty
+enorm -- enormous
+duur -- expensive
+veerboot -- ferry boat
+vuurwerk -- fireworks
+tuin -- garden
+feestdag -- holiday
+uitverkocht -- sold out
+tijd doorbrengen -- spend time
+vergieten / morsen -- spill
+; Sayings
+Heel goed bevriend zijn -- As thick as thieves
+Het hemd is nader dan de rok -- Blood is thicker than water
+Behandel anderen zoals je door hen behandeld wilt worden -- Love your neighbour as yourself
+Goede vrienden zijn er altijd voor je -- The road to a friend’s house is never long
+Je kunt je vrienden uitzoeken maar je familie niet -- You can choose your friends, but you can’t choose your family
+; Lesson 01
+geven om -- care about
+dagelijks -- daily
+anders -- different
+moeite -- effort
+in verlegenheid brengen -- embarrass
+geheel, gehele -- hole
+opwinding -- excitement
+ervaring -- experience
+veranda -- front porch
+volhouden -- keep up
+ochtend -- morning
+familielid -- relative
+belonen -- reward
+uitslapen -- sleep in
+besparen -- spare
+zwembroek -- swim trunks
+tiener -- teenager
+sensatie -- thrill
+zwaaien -- wave
+dragen (van kleding) -- wear
+bekend(e) -- well-known
+actrice -- actress
+vriend(in) -- friend
+anderen -- others
+ouders -- parents
+restaurant -- restaurant
+universiteit -- university
 `;
 
 const assignments = new Assignments(raw);
