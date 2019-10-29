280d1ca6144e4a2b69d58960a6751b9186776172
Matthijs van Henten <matthijs@ischen>
2019-10-29 22:30:42 +0000

    meer woordjes

--- js/data/en.js
+++ js/data/en.js
@@ -1,5 +1,55 @@
 import Assignments from "../lib/assignments.js";
 
+const Stone123 = `
+; Stone 1: Inviting people
+Ik vroeg me af of je zin hebt om vrijdag mee te gaan naar de drive-in bioscoop -- I was wondering if you'd like to go to the drive-in cinema on Friday
+Zou je volgende week met mij naar het schoolgala willen gaan -- Would you like to join me at the prom next week
+Ja, dat is goed -- Yes, I would
+Nee, dat wil ik niet -- No, I wouldn't
+Heb je zin om na school samen te gaan winkelen -- Do you fancy going shopping together after school
+Heb je interesse om te lasergamen vandaag -- Would you be interested in having a laser-tag match today
+Ik zou graag met je mee willen naar het pianoconcert dit weekend -- I'd like to accompany you to the piano recital this weekend
+We nemen je heel graag mee naar het filmfestival in het park morgen -- We'd be delighted to take you to the film festival in the park tomorrow
+Dank je. Ik zou heel graag willen komen -- Thank you. I'd love to come
+Goed idee! Wij wonen het festival graag bij -- Great idea! we'd like to attend the festival
+Sorry, maar ik kan niet -- I'm sorry, but I can't
+Helaas, ik ben niet in staat te komen -- Unfortunately, I'm unable to make it
+; Stone 2: Asking for and giving permission
+Mag ik wat virenden uitnodigen -- Can I invite some friends over
+Kunnen we deze zaal huren voor een afstudeerfeestje -- Could we rent this venue for a graduation party
+Mag ik het hoofdgerecht bestellen alstublieft -- Can I order the main course, please?
+Heb ik toestemming om volgende week naar het school gala te gaan -- Am I allowed to attend the school dance next week
+Is het goed als wij zondag een slaapfeestje geven -- Would it be all right if we throw a slumber party on Friday
+Ja dat is prima -- Yes that's fine
+Natuurlijk. Dat is goed -- Of course. That'd be all right
+Ik vroeg me af of ik je fiets vanavond mag lenen -- I was wondering if I could use your bike tonight
+Ik hoopte dat je me dit weekend naar het concert zou laten gaan -- I was hoping you would let me attend the concert this weekend
+Mag ik een evenement organiseren op deze locatie -- May I organise an event at this location
+Is het toegestaan om hier foto's te maken -- Is it permitted to take photographs here
+Sorry, dat is niet mogelijk -- I'm sorry, that's not possible
+Nee, dat mag je niet. Je hebt de volgende dag een toets -- No, you can't. you have a test the next day
+Ga je gang. Dat is prima -- Go ahead. That would be fine
+Natuurlijk mag je dat. Geen probleem -- Of course you can. No problem
+; Stone 3: Making plans and reservations
+Wat ga je vanavond doen -- What are you going to do tonight
+Wat zijn je plannen voor morgen -- What are your plans for tomorrow
+Waarom gaan we niet samen iets doen -- Why don't we hang out together
+Wil je naar dit feestje to -- Do you want to go to this party
+Ik zou graag willen reserveren voor donderdag avond -- I'd like to make a reservation for Thursday evening
+Ik bel om een tafel voor twee personen te reserveren -- I'm calling to book a table for two
+Kan ik kaarten reserveren voor de hogesnelheidstrein naar Parijs -- Could I reserve tickets for the high-speed train to Paris
+Is het nog steeds mogelijk om twee kaartjes te kopen met vroegboekkorting -- Is it still possible to purchase tickets with early-bird discount
+Je moet de kaartjes om half acht ophalen -- You need to pick up the tickets at half past seven
+Je moet een half uur voordat de film begint aanwezig zijn -- You ought to be there half an hour before the film starts
+De vertoning begint om vijf uur, dus zullen we de bus van vier uur nemen -- The screening begins at five o'clock, so shall we get the four o'clock bus
+De volleybalwedstrijd begint om half zeven, dus ik heb om drie uur met mijn vrienden afgesproken -- The volleyball match starts at half past six, so I'm meeting my friends at 3pm
+De signeersessie is om 16.30 uur, dus we moeten om half vier vertrekken -- The book signing is at 4.30pm, so we should leave at half past three
+Ik ga naar een comedy-improvisatieshow. Het wordt hilarisch -- I'm going to see an improv comedy show. It will be hilarious
+We gaan meedoen aan een recordpoging. het wordt de beste nacht ooit -- We're going to participate in a record attempt. It'll be the greatest night ever
+We kunnen de metro naar huis nemen. Hij vertrekt om half twaalf -- We could take the metro home. It leaves at half past eleven
+We moeten de bus nemen naar de bioscoop. Hij doet er maar vijftien minuten over -- We have to catch the bus to the cinema . It only takes fifteen minutes
+`;
+
 const raw = `
 ; Family
 tante -- aunt
@@ -192,6 +242,6 @@ borden -- plates
 bestek -- cutlery
 tafel -- table`;
 
-const assignments = new Assignments(raw);
+const assignments = new Assignments(Stone123+raw);
 
 export default assignments;
--- js/data/fr.js
+++ js/data/fr.js
@@ -1,6 +1,172 @@
 import Assignments from "../lib/assignments.js";
 
-const raw = `
+const unite2 = `
+; Unité 2: Apprendre 1
+het flatgebouw -- l'immeuble
+het appertement, de flat -- l'appartement
+het huis -- la maison
+de strat -- la rue
+het plein -- la place
+de boulevard -- le boulevard
+de buitenwijken -- la banlieue
+de buurt -- le quartier
+de toren -- la tour
+het station -- la gare
+de metro -- le métro
+de bus -- le bus
+de fiets -- le vélo
+de boot -- le bateau
+de auto -- la voiture
+de bushalte -- l'arrêt de bus
+lopen -- aller à pied
+gaan -- aller
+; Unité 2: Apprendre 2
+wonen -- habiter
+op -- sur
+ons, onze -- notre
+net als, zoals -- comme
+we hebben -- on a
+de woonkamer -- la salle de séjour
+de keuken -- la cuisine
+de badkamer -- la salle de bains
+de slaapkamer -- la chambre
+groot -- grand, grande
+bij jullie -- chez vous
+dus, dan -- alors
+klein -- petit, petite
+er is, er zijn -- il y a
+de tuin -- le jardin
+het water -- l'eau
+de moeder -- la mère
+zij heeft gemaakt -- elle a fait
+leuk, mooi -- joli, jolie
+houden van, het fijn vinden -- aimer
+het uitzicht -- la voiture
+de buren -- les voisins
+ver -- loin
+zetten -- mettre
+heel hard -- très fort
+; Unité 2: Apprendre 3
+De vriend van Nathan -- Le copain de Nathan
+De vrienden van Nathan -- Les copains de Nathan
+Het huis in de straat -- La maison dans la rue
+De huizen in de straat -- Les maisons dans la rue
+De auto op de snelweg -- L'auto sur l'autoroute
+De auto's op de snelweg -- Les autos sur l'autoroute
+Er is een tuin -- Il y a un jardin
+Er zijn tuinen -- Il y a des jardins
+Er is een huis -- Il y a une maisons
+Er zijn huizen -- Il y a des maisons
+; Unité 2: Apprendre 4
+Ik heb een afspraak -- J'ai rendez-vous
+Ik neem de bus -- Je prends le bus
+Waar is de halte -- Où est l'arrêt
+Het is te ver om te lopen -- A pied, c'est trop loin
+Je neemt een taxi -- Tu prends un taxi
+Waar ga je naartoe -- Tu vas où
+We gaan naar jouw huis -- On va chez toi
+Ik ben te laat -- Je suis en retard
+We gaan -- On y va
+; Unité 2: Apprendre 5
+de metro -- le métro
+tot later -- à plus
+de afspraak -- le rendez-vous
+het station -- la gare
+wat is er -- qu'est-ce qu'il y a
+de bus -- le bus
+waar -- où
+de halte -- l'arrêt
+natuurlijk -- bien sûr
+geen bus -- pas de bus
+de fiets -- le vélo
+lopend -- à pied
+te -- trop
+ver -- loin
+er is, er zijn -- il y a
+alles -- tout
+het huis -- la maison
+na, daarna -- après
+klimmen, instapppen -- monter
+naar jouw huis, bij jou thuis -- chez toi
+vervolgens -- puis
+op -- sur
+het bed -- le lit
+de trein -- le train
+; Unité 2: Apprendre 6
+Daar is een jongen -- Voilà un garçon
+Hij is aardig -- Il est sympa
+Daar is een meisje -- Voilà un fille
+Zij is aardig -- Elle est sympa
+Daar is de boot -- Voilà le batea
+Hij is groot -- Il est grand
+Daar is het huis -- Voilà la maison
+Zij is groot -- Elle est grande
+Er staat een fiets in de tuin -- Il y a un vélo dans le jardin
+Er is een grote keuken -- Il y a une grande cuisine
+Er zijn drie slaapkamers -- Il y a trois chambres
+; Unité 2: Apprendre 7
+welk, welke -- quel, quelle
+de stad -- la ville
+oké -- d'accord
+de straat - la rue
+de verdieping, de etage -- l'étage
+de lift -- l'ascenseur
+de trap -- l'escalier
+moeilijk, zwaar, hard -- dur
+de kamer, de slaapkamer -- la chambre
+prachtig -- magnifique
+groot - grand, grande
+de plaats -- la place
+zelfs -- même
+de hoek -- le coin
+het boek -- le livre
+houden van, mooi vinden -- aimer
+het balkon -- le balcon
+kijk -- regarde
+hoog -- haut, haute
+hier -- ici
+mooi, leuk -- joli, jolie
+het raam -- la fenêtre
+tussen -- entre
+de boom -- l'arbre
+leuk -- chouette
+erg, heel -- très
+rustig -- calme
+; Unité 2: Apprendre 8
+Ik woon in de Gérardstraat 10 -- J'habite 10, rue Gérard
+Op welke verdieping is het -- C'est à quel étage
+Hier is mijn kamer -- Voilà ma chambre
+Ik heb ook een balkon -- J'ai aussi un balcon
+Ik heb uitzicht op het park -- J'ai vue sur le parc
+We lopen naar huis -- On rentre à pied
+Ik woon vlakbij -- J'habite tout près
+Hoe is dat? -- Il est comment?
+Hoeveel kamers hebben jullie -- Vous avez combien de pièces
+Er is een grote tuin -- Il y a un grand jardin
+; Unité 2: Apprendre 9
+vandaag -- aujourd'hui
+samen -- ensemble
+vlakbij -- tout près
+ik bren door -- je passe
+de vader -- le père
+pretti gweekend -- bon week-end
+de buurt -- le quartier
+het winkelcentrum -- le centre commercial
+de flat, het appertement -- l'appartement
+jullie hebben -- vous avez
+hoeveel -- combien
+de kamer, het vertrek -- la pièce
+de keuken -- la cuisine
+de woonkamer -- la salle de séjour
+eigen -- propre
+wat een geluk -- quelle chance
+klein -- petit, petite
+de tuin -- le jardin
+hij heeft -- il a
+de auto -- la voiture
+`;
+
+const unite1 = `
 ; Unité 1: Apprendre 1 & 2
 twee meisjes -- deux filles 
 twee jongens -- deux garçons 
@@ -38,7 +204,7 @@ zij heet  -- elle s'appelle
 hoe heet hij -- comment il s'appelle
 hij heet  -- il s'appelle
 dit is Tim -- voilà Tim
-; Unité 2: Apprendre 5
+; Unité 1: Apprendre 5
 de tas  -- le sac
 je hebt, heb je			  --		tu as 
 het problem   -- le problème 
@@ -89,6 +255,6 @@ met -- avec
 veel succes -- bonne chance
 `;
 
-const assignments = new Assignments(raw);
+const assignments = new Assignments(unite2 + unite1);
 
 export default assignments;
