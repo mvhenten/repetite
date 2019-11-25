a612a579b5017d13a662af22b3730999e9bca5d2
Matthijs van Henten <matthijs@ischen>
2019-11-25 23:03:23 +0000

    move components to files

---
+++ js/lib/component/form/answer.js
@@ -0,0 +1,43 @@
+import { html, Component } from 'https://unpkg.com/htm/preact/standalone.module.js';
+
+class AnswerForm extends Component {
+    state = {};
+
+    constructor({ onSubmit }) {
+        super();
+        this.doOnSubmit = onSubmit;
+    }
+
+    onChange = (evt) => {
+        const { value } = evt.target;
+        this.setState({ value });
+    }
+
+    onSubmit = (evt) => {
+        evt.preventDefault();
+        this.doOnSubmit(this.state);
+    }
+
+    componentDidMount() {
+        this.el.focus();
+    }
+
+    render({ onSubmit }) {
+        return html `
+        <form id=${Math.random()} onSubmit=${this.onSubmit} class="answers">
+            <input value=${this.state.value} 
+                ref=${i => this.el = i} id=${Math.random()} 
+                onInput=${this.onChange} 
+                autoFocus="true" type="text" 
+                autocomplete="off" 
+                autocorrect="off" 
+                autocapitalize="off" 
+                spellcheck="false" 
+                name="answer" 
+                placeholder="antwoord" />
+        </form>
+    `;
+    }
+}
+
+export default AnswerForm;
---
+++ js/lib/component/form/assignments.js
@@ -0,0 +1,86 @@
+import { html, Component } from 'https://unpkg.com/htm/preact/standalone.module.js';
+
+const RadioButton = ({ name, checked, label, value, onChange }) => {
+    const attr = {
+        id: [name, value].join("-"),
+        name,
+        value,
+        type: 'radio',
+        onChange,
+    };
+
+    if (value == checked)
+        attr.checked = "checked";
+
+    return html `
+        <label for="${attr.id}">
+            <input ...${attr} />
+            ${label || name}
+        </label>
+    `;
+};
+
+
+class AssignmentsForm extends Component {
+    state = { value: "" }
+
+    constructor({ assignments, appState }) {
+        super();
+        this.appState = appState;
+        this.assignments = assignments;
+    }
+
+    get selected() {
+        let { value } = this.state;
+        return value || this.options[0];
+    }
+
+    get options() {
+        return Array.from(this.assignments.keys);
+    }
+
+    get mode() {
+        return this.state.mode || "test";
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const { mode, selected } = this;
+        const words = this.assignments.getWords(selected);
+
+        this.appState.emit("selectAssignment", { mode, selected, words });
+    }
+
+    onSelectAssignment = evt => {
+        const { value } = evt.target;
+        this.setState({ value });
+    }
+
+    onSetMode = evt => {
+        const { value } = evt.target;
+        this.setState({ mode: value });
+    }
+
+    render() {
+        return html `
+            <form onSubmit=${this.onSubmit} id="assignments">
+                <label>
+                    Select            
+                    <select onChange=${this.onSelectAssignment} id="assignments-assignment" name="assignment">
+                    ${this.options.map((header) => {
+                        return html `<option value=${header}>${header}</option>`;
+                    })}
+                    </select>
+                </label>
+                <div class="text-medium padding-1 spaced-2x">
+                    <${RadioButton} name="mode" value="test" checked=${this.mode} onChange=${this.onSetMode} label="Testen" />
+                    <${RadioButton} name="mode" value="practice" checked=${this.mode} onChange=${this.onSetMode} label="Oefenen" />
+                </div>
+                <input type="submit" value="start" />
+            </form>
+        `;
+    }
+}
+
+export default AssignmentsForm;
